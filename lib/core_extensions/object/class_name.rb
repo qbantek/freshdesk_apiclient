676c6616e44de451ee84ab3fb5898b5211453426
Erich Quintero <erich@financialapps.com>
2017-03-09 16:52:47 -0500

    using extensions instead of module

--- lib/core_extensions/object/class_name.rb
+++ lib/core_extensions/object/class_name.rb
@@ -1,20 +1,22 @@
 # frozen_string_literal: true
 module ObjectExtensions
-  refine Object do
-    def class_name
-      full_class_name_as_array.last
-    end
+  unless Object.new.respond_to?(:class_name)
+    refine Object do
+      def class_name
+        full_class_name_as_array.last
+      end
 
-    def namespace_as_array
-      full_class_name_as_array[0...-1]
-    end
+      def namespace_as_array
+        full_class_name_as_array[0...-1]
+      end
 
-    def full_class_name_as_array
-      full_class_name.split('::')
-    end
+      def full_class_name_as_array
+        full_class_name.split('::')
+      end
 
-    def full_class_name
-      self.class.name
+      def full_class_name
+        self.class.name
+      end
     end
-  end unless Object.new.respond_to?(:class_name)
+  end
 end
--- lib/core_extensions/string/camelize.rb
+++ lib/core_extensions/string/camelize.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 module StringExtensions
-  refine String do
-    def camelize
-      split(/[^a-z0-9]/i).map { |word| word.capitalize }.join
+  unless String.new.respond_to?(:camelize)
+    refine String do
+      def camelize
+        split(/[^a-z0-9]/i).map(&:capitalize).join
+      end
     end
-  end unless String.new.respond_to?(:camelize)
+  end
 end
